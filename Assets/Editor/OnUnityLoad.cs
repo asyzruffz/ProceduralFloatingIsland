8452a666c29dc7610783fbd5aa85d9c80b860b3c
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-02-25 19:39:19 +0800

    base

---
+++ Assets/Editor/OnUnityLoad.cs
@@ -0,0 +1,31 @@
+﻿// *******************************************************
+// Copyright 2013 Daikon Forge, all rights reserved under 
+// US Copyright Law and international treaties
+// *******************************************************
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class OnUnityLoad
+{
+    static OnUnityLoad()
+    {
+        EditorApplication.playmodeStateChanged = () =>
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+
+                Debug.Log("Auto-Saving scene before entering Play mode: " + EditorSceneManager.GetActiveScene().name);
+
+                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
